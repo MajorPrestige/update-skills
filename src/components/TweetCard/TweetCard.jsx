80888b630461120db29bcb4791cf8679f2a3677e
Denys Dashkevych <denysdashkevych@gmail.com>
2023-06-24 19:19:28 -0500

    create avatar

--- src/components/TweetCard/TweetCard.jsx
+++ src/components/TweetCard/TweetCard.jsx
@@ -1,15 +1,30 @@
 import Card from '../../images/card.png';
 import Card2x from '../../images/card@2x.png';
+import Avatar from '../../images/avatar.png';
+import Avatar2x from '../../images/avatar@2x.png';
 
 import s from './TweetCard.module.scss';
 
 const TweetCard = () => {
   return (
     <div className={s.card}>
-      <picture >
+      <picture>
         <source srcSet={Card2x} media="(min-resolution: 2dppx)" />
         <img className={s.img} src={Card} alt="card chat" />
       </picture>
+      <div className={s.info}>
+        <picture className={s.imgWrapper}>
+          <source srcSet={Avatar2x} media="(min-resolution: 2dppx)" />
+          <img height="62" width="62" className={s.ava} src={Avatar} alt="avatar" />
+        </picture>
+        <p className={s.tweets}>777 TWEETS</p>
+        <p>
+          <span className={s.tweets__number}>100500</span> FOLLOWERS
+        </p>
+        <button className={s.btn} type="button">
+          Follow
+        </button>
+      </div>
     </div>
   );
 };
