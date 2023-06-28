9f3c14fef8b1497c9eb73dd8477bcdee94369669
Denys Dashkevych <denysdashkevych@gmail.com>
2023-06-28 15:47:55 -0500

    create pagination

--- src/components/TweetList/TweetList.jsx
+++ src/components/TweetList/TweetList.jsx
@@ -1,21 +1,37 @@
-import TweetCard from 'components/TweetCard/TweetCard';
-import users from 'database/users.json';
+import { useState } from 'react';
 
-import s from './TweetList.module.scss'
+import TweetCard from 'components/TweetCard/TweetCard';
+import usersDb from 'database/users.json';
+
+import s from './TweetList.module.scss';
 
 const TweetList = () => {
+  const [users] = useState(usersDb);
+  const [displayedUsers, setDisplayedUsers] = useState(12);
+
+  const loadMoreUsers = () => {
+    setDisplayedUsers((prevCount) => prevCount + 12);
+  };
+
   return (
-    <ul className={s.list}>
-      {users.map((user) => (
-        <TweetCard
-          key={user.id}
-          name={user.user}
-          avatar={user.avatar}
-          followers={user.followers}
-          tweets={user.tweets}
-        />
-      ))}
-    </ul>
+    <>
+      <ul className={s.list}>
+        {users.slice(0, displayedUsers).map((user) => (
+          <TweetCard
+            key={user.id}
+            name={user.user}
+            avatar={user.avatar}
+            followers={user.followers}
+            tweets={user.tweets}
+          />
+        ))}
+      </ul>
+      {(users.length > displayedUsers || users.length !== displayedUsers) && (
+        <button type="button" className={s.btn} onClick={loadMoreUsers}>
+          load more
+        </button>
+      )}
+    </>
   );
 };
 
