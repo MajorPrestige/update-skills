cf88987d3fd14a7f332ac7d9850a78aa955808d1
Denys Dashkevych <denysdashkevych@gmail.com>
2023-07-16 17:38:10 -0500

    Update TweetsPage.jsx

--- src/pages/TweetsPage/TweetsPage.jsx
+++ src/pages/TweetsPage/TweetsPage.jsx
@@ -1,6 +1,17 @@
 import TweetList from 'components/TweetList/TweetList';
 import s from './TweetsPage.module.scss';
 
+
+var merge = function (nums1, m, nums2, n) {
+  const sortedFirstArr = nums1.splice(0, m)
+  const sortedSecondArr = nums2.splice(0, n)
+  const sortedThirdArr = [...sortedFirstArr, ...sortedSecondArr].sort((a, b) => a - b)
+  return sortedThirdArr;
+};
+
+console.log(merge([1,2,3,0,0,0], 3, [2,5,6], 3));
+
+
 const TweetsPage = () => {
   return (
     <div className={s.colorContainer}>
